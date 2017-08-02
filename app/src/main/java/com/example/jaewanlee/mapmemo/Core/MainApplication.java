7c44f121392f35e8cc8f4009b9557997c558dfb3
jaewan lee <jaewanlee@jaewanui-MacBook-Pro.local>
2017-08-02 17:35:55 +0900

    feat : home화면 추가

--- app/src/main/java/com/example/jaewanlee/mapmemo/Core/MainApplication.java
+++ app/src/main/java/com/example/jaewanlee/mapmemo/Core/MainApplication.java
@@ -1,8 +1,13 @@
 package com.example.jaewanlee.mapmemo.Core;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
+import com.example.jaewanlee.mapmemo.Database.UserDatabase;
 import com.tsengvn.typekit.Typekit;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 
 /**
@@ -11,9 +16,20 @@ import com.tsengvn.typekit.Typekit;
 
 public class MainApplication extends Application {
 
+    Realm UserData;
+    SharedPreferences sharedPreferences;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
         Typekit.getInstance().addNormal(Typekit.createFromAsset(this,"BMHANNA_11rs_otf.otf"));
     }
+
+    public void userDataInit(){
+        this.UserData=Realm.getInstance(this);
+        RealmResults<UserDatabase> userDatabases=this.UserData.where(UserDatabase.class).findAll();
+    }
+
+
 }
--- app/src/main/java/com/example/jaewanlee/mapmemo/Login/Login.java
+++ app/src/main/java/com/example/jaewanlee/mapmemo/Login/Login.java
@@ -1,15 +1,42 @@
 package com.example.jaewanlee.mapmemo.Login;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.jaewanlee.mapmemo.R;
 
 public class Login extends AppCompatActivity {
 
+    EditText userIdTextView;
+    EditText userPasswordTextView;
+    ImageButton logInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userIdTextView=(EditText)findViewById(R.id.login_id_editText);
+        userPasswordTextView=(EditText)findViewById(R.id.login_pass_editText);
+        logInButton=(ImageButton)findViewById(R.id.login_login_button);
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+    }
+
 }
